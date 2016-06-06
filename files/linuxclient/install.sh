3eeccc32e10f4c6b37049415c0105c5a238c0503
Tim Wells <timwells@westtek.com>
2016-06-06 09:44:22 -0700

    Issue #7
    remove openssl from client and server installs. Installer now assumes
    that openssl is installed first.

--- files/linuxclient/install.sh
+++ files/linuxclient/install.sh
@@ -1,15 +1,63 @@
 #!/bin/bash
 
-# move required files to a common location
-mkdir /usr/local/ryft
-cp -avr * /usr/local/ryft
+if [ -x /usr/bin/clear ]; then
+    /usr/bin/clear
+fi
+cat <<EOF
+Installing Ryft Linux ODBC Client Driver
+EOF
 
-# configure odbc ini files
-cat odbc.ini >> ~/.odbc.ini
-chmod 666 ~/.odbc.ini
-cp simbaclient.ini ~/.simbaclient.ini
-chmod 666 ~/.simbaclient.ini
+if [ "$(id -u)" == "0" ]; then
+    defpath="/usr/local"
+else
+    defpath="$HOME"
+fi
+
+echo -n "Install to directory [$defpath]: "
+read path
+if [ "$path" == "" ]; then
+    path="$defpath"
+fi
+if [ ! -d "$path" ]; then
+    mkdir $path
+fi
+fullpath=`echo $path/ryft | sed 's,//,/,g'`
+if [ ! -d "$fullpath" ]; then
+    mkdir $fullpath
+fi
+
+echo -n "What is the IP address of the Ryft ODBC Server [127.0.0.0]? "
+read ipaddr
+if [ "$ipaddr" == "" ]; then
+    ipaddr="127.0.0.0"
+fi
+
+echo -n "What is the IP port of the Ryft ODBC Server [7409]? "
+read port
+if [ "$port" == "" ]; then
+    port="7409"
+fi
+
+cat odbc.template | sed "s#_INSTALLDIR_#$fullpath#;s#_IPADDR_#$ipaddr#;s#_PORT_#$port#" > odbc.ini
+cat odbcinst.template | sed "s#_INSTALLDIR_#$fullpath#" > odbcinst.ini
+cat simbaclient.template | sed "s#_INSTALLDIR_#$fullpath#" > simbaclient.ini
+cat r.ld.so.conf.template | sed "s#_INSTALLDIR_#$fullpath#" > r.ld.so.conf
+
+# move required files to a common location
+cp -avr * $fullpath
+mv $fullpath/simbaclient.ini $HOME/.simbaclient.ini
 
 # update shared library cache
-cp r.ld.so.conf /etc/ld.so.conf.d
-ldconfig
+if [ "$(id -u)" == "0" ]; then
+    cp r.ld.so.conf /etc/ld.so.conf.d
+    ldconfig
+fi
+
+echo -n "Installation complete... [enter] "
+read foo
+
+if [ -x /usr/bin/clear ]; then
+    /usr/bin/clear
+fi
+
+cat README
