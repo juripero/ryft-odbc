72ccf90bc041437ac98dd0f22cd3b93d7051a121
Al Leyva <al.leyva@ryft.com>
2019-06-12 10:12:03 -0400

    Update build_linuxclient.sh

--- build_linuxclient.sh
+++ build_linuxclient.sh
@@ -7,6 +7,7 @@ RELEASE=20.0
 rm -r $RELDIR
 mkdir $RELDIR
 mkdir $RELDIR/lib
+mkdir $RELDIR/lib/x8664
 mkdir $RELDIR/errormessages
 mkdir $RELDIR/errormessages/en-US
 cp -av ~/development/SimbaEngineSDK/10.0/DataAccessComponents/ThirdParty/icu/53.1/centos5/gcc4_4/release64/lib/*.so* $RELDIR/lib/x8664
@@ -27,6 +28,6 @@ cp -av ~/development/ryft-odbc/files/linuxclient/VERSION $RELDIR
 chmod 666 $RELDIR/VERSION
 cp -av ~/development/ryft-odbc/files/linuxclient/install.sh $RELDIR
 chmod 777 $RELDIR/install.sh
-tar -czf ~/development/RELDIR-$VERSION-$RELEASE.tar.gz $RELDIR
+tar -czf ~/development/$RELDIR-$VERSION-$RELEASE.tar.gz $RELDIR
 rm -r $RELDIR
 
